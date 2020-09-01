b307e4292edafa562dd24df07ed30af35347293f
Svitlana Galianova <Svitlana_Galianova@manulife.com>
2020-09-01 00:14:11 -0400

    module 2

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
 
 const app = express();
 const PORT = 4000;
 
 // bodyparser setup
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json()); 
+app.use(bodyParser.json());
+
+try {
+    await mongoose.connect('mongodb://localhost/productsdb', { useNewUrlParser: true, useUnifiedTopology: true });
+} catch (error) {
+    console.log(error);
+}
+mongoose.Promise = global.Promise;
 
 app.get('/', (req, res) =>
     res.send(`Store server running on port ${PORT}`)
 );
 
-app.listen(PORT, () => 
+app.listen(PORT, () =>
     console.log(`Your server is running on port ${PORT}`)
 );
