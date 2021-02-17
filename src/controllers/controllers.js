d53f2330544aaac99a2ad950215bc1f13d4a4a87
Svitlana Galianova <Svitlana_Galianova@manulife.com>
2021-02-17 16:41:29 -0500

    module 5

--- src/controllers/controllers.js
+++ src/controllers/controllers.js
@@ -1,5 +1,4 @@
 import mongoose from 'mongoose'
-import { reset } from 'nodemon'
 
 import { ProductSchema } from '../models/models'
 
@@ -28,6 +27,23 @@ export const getProductWithID = (req, res) => {
     Product.findById(req.params.ProductID, (err, Product) => {
         if (err)
             res.send(err)
+        res.json(Product);
+    });
+}
+
+export const updateProduct = (req, res) => {
+    Product.findOneAndUpdate({ _id: req.params.ProductID }, req.body, { new: true, useFindAndModify: false }, (err, Product) => {
+        if (err)
+            res.send(err)
         res.json(Product)
-    })
+    });
+}
+
+export const deleteProduct = (req, res) => {
+    Product.deleteOne({ _id: req.params.ProductID }, (err, Product) => {
+        if (err) {
+            res.send(err);
+        }
+        res.json({ message: 'successfully deleted product' });
+    });
 }
