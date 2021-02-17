8ef7c09f5b2980d81b65a2383f236b01ea2e5562
Svitlana Galianova <Svitlana_Galianova@manulife.com>
2021-02-17 15:43:13 -0500

    module 4

--- src/controllers/controllers.js
+++ src/controllers/controllers.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose'
+import { reset } from 'nodemon'
 
 import { ProductSchema } from '../models/models'
 
@@ -8,6 +9,23 @@ export const addnewProduct = (req, res) => {
     var newProduct = new Product(req.body)
 
     newProduct.save((err, Product) => {
+        if (err)
+            res.send(err)
+        res.json(Product)
+    })
+}
+
+export const getProducts = (req, res) => {
+    Product.find({}, (err, Product) => {
+        if (err)
+            res.send(err)
+        res.json(Product)
+    })
+
+}
+
+export const getProductWithID = (req, res) => {
+    Product.findById(req.params.ProductID, (err, Product) => {
         if (err)
             res.send(err)
         res.json(Product)
